073455f47065b89126eb3711ef4304566e85d42a
tangwei <409766394@qq.com>
2020-09-17 19:48:43 +0800

    second commit

--- app/app.go
+++ app/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
 	"log"
 	"net/http"
 )
@@ -12,11 +13,12 @@ import (
 // app encapsulates Env, Router and middleware
 
 type App struct {
-	Router *mux.Router
+	Router      *mux.Router
+	Middlewares *Middleware
 }
 
 type shortenReq struct {
-	URL                 string `json:"url" validate:"nonzero"`
+	URL                 string `json:"url" validate:"required"`
 	ExpirationInMinutes int64  `json:"expiration_in_minutes" validate:"min=0"`
 }
 
@@ -30,25 +32,30 @@ func (a *App) Initialize() {
 	// set log for formatter
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	a.Router = mux.NewRouter()
+	a.Middlewares = &Middleware{}
 	a.initializeRoutes()
 }
 
 func (a *App) initializeRoutes() {
-	a.Router.HandleFunc("/api/shorten", a.createShortLink).Methods("POST")
-	a.Router.HandleFunc("/api/info", a.getShortLinkInfo).Methods("GET")
-	a.Router.HandleFunc("/{shortlink:[a-zA-Z0-9]{1,11}}", a.redirect).Methods("GET")
+	//a.Router.HandleFunc("/api/shorten", a.createShortLink).Methods("POST")
+	//a.Router.HandleFunc("/api/info", a.getShortLinkInfo).Methods("GET")
+	//a.Router.HandleFunc("/{shortlink:[a-zA-Z0-9]{1,11}}", a.redirect).Methods("GET")
+	m := alice.New(a.Middlewares.LoggingHandler, a.Middlewares.RecoverHandler)
+	a.Router.Handle("/api/shorten", m.ThenFunc(a.createShortLink)).Methods("POST")
+	a.Router.Handle("/api/info", m.ThenFunc(a.getShortLinkInfo)).Methods("GET")
+	a.Router.Handle("/{shortlink:[a-zA-Z0-9]{1,11}}", m.ThenFunc(a.redirect)).Methods("GET")
 }
 
 // createShortLink 长地址转换为短地址
 func (a *App) createShortLink(w http.ResponseWriter, r *http.Request) {
 	var req shortenReq
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		log.Println("fail to decode body, err:", err)
+		responseWithError(w, StatusError{http.StatusBadRequest, fmt.Errorf("parse parameters failed %v", r.Body)})
 		return
 	}
 	validate := validator.New()
 	if err := validate.Struct(req); err != nil {
-		log.Println("fail to validate req, err:", err)
+		responseWithError(w, StatusError{http.StatusBadRequest, fmt.Errorf("validate parameters failed %v", req)})
 		return
 	}
 	defer r.Body.Close()
@@ -70,4 +77,23 @@ func (a *App) redirect(w http.ResponseWriter, r *http.Request) {
 func (a *App) Run(addr string) {
 	log.Printf("start listenning")
 	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
+
+func responseWithError(w http.ResponseWriter, err error) {
+	switch e := err.(type) {
+	case Error:
+		log.Printf("HTTP %d - %s", e.Status(), e)
+		responseWithJSON(w, e.Status(), e.Error())
+	default:
+		responseWithJSON(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+	}
+}
+
+func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	resp, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	w.Write(resp)
 }
